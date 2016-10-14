01922423cf06c2a5aeaa88232c57611800b246d2
zhouzhi <527195016@qq.com>
2016-10-14 20:19:09 +0800

    10.14

--- Assignment 2/DataProcessing.py
+++ Assignment 2/DataProcessing.py
@@ -6,6 +6,7 @@ import ReadFile
 import LearnPCAConjMat
 import DimReduction
 import PredictLabel
+import ISOMAP
 
 trainfilename = 'sonar-train.txt'
 testfilename = 'sonar-test.txt'
@@ -24,3 +25,6 @@ u, s, v = linalg.svd(trainmatdata) #根据训练集学习SVD投影矩阵
 finaltraindataSVD, finaltestdataSVD = DimReduction.dimReduction(trainmatdata, testmatdata, u, dimension) #对训练集和测试集数据进行降维
 accuracySVD = PredictLabel.predictLabel(finaltraindataSVD, trainlabel, finaltestdataSVD, testlabel) #对测试数据集进行标签预测，计算正确率
 print accuracySVD
+
+k = 4 #k-NN参数k
+ISOMAP.ISOMAP(trainmatdata, testmatdata, k)
--- Assignment 2/ISOMAP.py
+++ Assignment 2/ISOMAP.py
@@ -10,4 +10,9 @@ from numpy import *
 
 '''
 
-def ISOMAP(trainmatdata, testmatdata):
+def ISOMAP(trainmatdata, testmatdata, k):
+    nodedata = hstack((trainmatdata, testmatdata)) #矩阵合并
+    nodenum = size(nodedata, 1) #节点数，即数据量
+    wgraph = zeros((nodenum, nodenum)) - 1 #节点的连接权重矩阵，初始值都设为-1
+    for num in range(0, nodenum, 1): #对角线设为0
+        wgraph[num][num] = 0
